bf56e8d1d6568261f24e02a8065913ff4b59f351
Amin Saffarnejad <vira1368@gmail.com>
2025-12-01 23:25:07 +0330

    Add models.

---
+++ solutions/C#/saffarnejad/UserManagementSystem/UserManagementSystem.API/Models/UserRegistrationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManagementSystem.API.Models
+{
+    public class UserRegistrationRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public IFormFile? Document { get; set; }
+    }
+}
---
+++ solutions/C#/saffarnejad/UserManagementSystem/UserManagementSystem.API/Models/UserRegistrationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManagementSystem.API.Models
+{
+    public class UserRegistrationResponse
+    {
+        public Guid UserId { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
